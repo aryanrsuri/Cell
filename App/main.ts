b8a09a4c06393efb6ba57e36ce3567e752894128
aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>
2023-07-03 23:34:23 -0700

    g
    Signed-off-by: aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>

--- App/main.ts
+++ App/main.ts
@@ -1,6 +1,8 @@
 import { serve } from "https://deno.land/std@0.192.0/http/server.ts";
 // import { serveFile } from "https://deno.land/std@0.140.0/http/file_server.ts";
-const HTML = await Deno.readFile(`${Deno.cwd()}/index.html`);
+const HTML = await Deno.readFile(
+  "/Users/aryansuri/Software/Cell/App/index.html",
+);
 
 // serve(async (req) => {
 //   return await serveFile(req, `${Deno.cwd()}/index.html`);
