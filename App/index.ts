aa53df4ab987159d37408511e1e483fb0e501653
aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>
2023-07-03 23:14:51 -0700

    swapped to deno instead
    Signed-off-by: aryanrsuri <13823329+aryanrsuri@users.noreply.github.com>

--- App/index.ts
+++
@@ -1,9 +0,0 @@
-Bun.serve({
-  fetch(request: Request):
-    | Response
-    | Promise<Response> {
-    return new Response(Bun.file("./index.html"));
-  },
-
-  port: process.env.PORT || 3000,
-});
